5a48631c89f6f38d0f1db993b92a2aba5a895daa
vijaytank130@gmail.com <vijaytank130@gmail.com>
2023-11-19 02:59:43 +0530

    frontend commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [response, setResponse] = useState('');
+
+  const handleSubmit = async () => {
+    try {
+      const result = await axios.post('http://localhost:8080/api/devgpt', { input });
+      setResponse(result.data.response);
+    } catch (error) {
+      console.error('Error calling ChatGPT API:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      <button onClick={handleSubmit}>Submit</button>
+      <div>
+        <p>Response:</p>
+        <p>{response}</p>
+      </div>
     </div>
   );
 }
